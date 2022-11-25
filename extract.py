403b9c7b2a1f12d1a2e13949f0c76fbfe0bed82f
Your Name <logan.aker1989@gmail.com>
2022-11-25 11:33:42 -0500

    Fixing file creation issues.

--- extract.py
+++ extract.py
@@ -1,6 +1,6 @@
 import os
-with open(r"ip.tmp", 'r') as fp:
+with open(r'ip', 'r') as fp:
     for count, line in enumerate(fp):
         pass
 total = count + 1
-os.remove("ip.tmp")
+os.remove('ip')
