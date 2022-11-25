574fd3e1206575f9244426fff27cfb9017541fbe
Your Name <logan.aker1989@gmail.com>
2022-11-25 10:05:47 -0500

    Added TestMe method.

--- main.py
+++ main.py
@@ -6,93 +6,52 @@ GPIO.setmode(GPIO.BCM)
 A = 17
 B = 27
 C = 22
+class Display:
+  def __init__(self, digit: int, segments: list):
+    self.digit = digit
+    self.segments = segments
 D = 23
 E = 24
 F = 25
 G = 19
 DP = 6
 
-#ZERO = [A, B, C, D, E, F]
-ONE = [B, C]
-TWO = [A, B, D, E, G]
-THREE = [A, B, C, D, G]
-FOUR = [B, C, F, G]
-FIVE = [A, C, D, F, G]
-SIX = [A, C, D, E, F, G]
-SEVEN = [A, B, C]
-EIGHT = [A, B, C, D, E, F, G]
-NINE = [A, B, C, F, G]
-ALL = [A, B, C, D, E, F, G, DP]
-
 class Display:
   def __init__(self, digit: int, segments: list):
     self.digit = digit
     self.segments = segments
 
-def Prep():
-  for segments in ALL:
-    GPIO.setup(segments, GPIO.OUT)
-
-#def Zero():
-#  for p in ZERO:
-#    GPIO.output(p, GPIO.LOW)
-    
-def One():
-  for p in ONE:
-    GPIO.output(p, GPIO.LOW)
-    
-def Two():
-  for p in TWO:
-    GPIO.output(p, GPIO.LOW)
-    
-def Three():
-  for p in THREE:
-    GPIO.output(p, GPIO.LOW)
-    
-def Four():
-  for p in FOUR:
-    GPIO.output(p, GPIO.LOW)
-
-def Five():
-  for p in FIVE:
-    GPIO.output(p, GPIO.LOW)
-    
-def Six():
-  for p in SIX:
-    GPIO.output(p, GPIO.LOW)
-    
-def Seven():
-  for p in SEVEN:
-    GPIO.output(p, GPIO.LOW)
-    
-def Eight():
-  for p in EIGHT:
-    GPIO.output(p, GPIO.LOW)
-    
-def Nine():
-  for p in NINE:
-    GPIO.output(p, GPIO.LOW)
-
-def All():
-  for p in ALL:
-    GPIO.output(p, GPIO.LOW)
-
-def Clear():
-  for p in ALL:
-    GPIO.output(p, GPIO.HIGH)
-
 ZERO = Display(0, [A, B, C, D, E, F])
+ONE = Display(1, [B, C])
+TWO = Display(2, [A, B, D, E, G])
+THREE = Display(3, [A, B, C, D, G])
+FOUR = Display(4, [B, C, F, G])
+FIVE = Display(5, [A, C, D, F, G])
+SIX = Display(6, [A, C, D, E, F, G])
+SEVEN = Display(7, [A, B, C])
+EIGHT = Display(8, [A, B, C, D, E, F, G])
+NINE = Display(9, [A, B, C, F, G])
+ALL = Display(10, [A, B, C, D, E, F, G, DP])
 
-MyNum = int(0)
 
-def Check():
-  if ZERO.digit == MyNum:
-    print('ZERO')
-  else:
-    print('Somethig done got fuckered.')
+def Prep():
+  GPIO.setup(ALL.segments, GPIO.OUT)
+
+def TestMe():
+  print(ALL.segments)
+
+#MyNum = int(0)
+
+#def Check():
+#  if ZERO.digit == MyNum:
+#    print('ZERO')
+#  else:
+#    print('Somethig done got fuckered.')
+#
 
 Prep()
-Check()
+TestMe()
+#Check()
 #Zero()
 #sleep(1)
 #Clear()
